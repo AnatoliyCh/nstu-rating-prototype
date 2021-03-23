a8c41c086fb9be9384f3d0fd24986a6a423001f9
Anatoly <googlsocial97@gmail.com>
2021-03-23 17:19:19 +0700

    обновление апи

--- apps/common/config.ts
+++
@@ -1,5 +0,0 @@
-// API
-export const PATH_BASE = "http://";
-export const PATH_PROXY = "https://cors-anywhere.herokuapp.com/"; // прокси для получения CORS
-export const PATH_HOST = "5.130.14.37:8080/"; // сервер для запросов
-export const PATH_PROXY_HOST = PATH_PROXY + PATH_HOST; // прокси + хост
