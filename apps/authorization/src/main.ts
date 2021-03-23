28adac09f5869d07c9f58eec2a2c08567ff777c7
Anatoly <googlsocial97@gmail.com>
2021-03-23 13:29:29 +0700

    Update main.ts

--- apps/authorization/src/main.ts
+++ apps/authorization/src/main.ts
@@ -5,11 +5,10 @@ Vue.config.productionTip = false;
 
 // подключение контроллов
 import "ant-design-vue/dist/antd.css";
-import { Button, Input, Icon, Checkbox, Card } from "ant-design-vue";
+import { Button, Input, Icon, Card } from "ant-design-vue";
 Vue.use(Button);
 Vue.use(Input);
 Vue.use(Icon);
-Vue.use(Checkbox);
 Vue.use(Card);
 
 new Vue({
