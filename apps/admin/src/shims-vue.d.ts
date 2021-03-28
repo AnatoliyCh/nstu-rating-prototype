31d2028f7d01b8f06418ac67939c3758353b003e
Anatoly <googlsocial97@gmail.com>
2021-03-28 21:08:32 +0700

    локализация

--- apps/admin/src/shims-vue.d.ts
+++ apps/admin/src/shims-vue.d.ts
@@ -2,3 +2,13 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+// длч локализации
+interface ruRU {
+  [key: string]: any;
+}
+declare module "ant-design-vue/es/locale/ru_RU" {
+  import ruRU from "ant-design-vue/es/locale/ru_RU";
+  const ruRU: ruRU;
+
+  export default ruRU;
+}
